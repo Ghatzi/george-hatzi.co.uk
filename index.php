6e46be163688a4352bc6fc399fe2861b3ec6b063
G Hatzi <52458189+Ghatzi@users.noreply.github.com>
2020-07-29 16:34:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-define ('SITE_KEY', '6Lfeg6gUAAAAAHxMY1v4MYeT7dzZaXFI2esPRxyr');
+define ('SITE_KEY', '');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
